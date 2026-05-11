f9c12036333ffea2fb0af777f91ac8530dd92f6d
Diego Magnata <dfm@cesar.school>
2026-05-11 19:17:30 -0300

    feat: integrar pombo ao sistema de obstáculos
    - Adiciona include pigeon.h em obstacle.h
    - Novo tipo OBSTACLE_PIGEON no enum ObstacleType
    - Pombo adicionado ao union de dados
    - createObstacle() inicializa pombo com createPigeon()
    - updateObstacle() chama updatePigeon() para movimento
    - drawObstacle() renderiza pombo voador
    - Timer de fezes preparado para próximo passo

--- src/entities/obstacle.c
+++ src/entities/obstacle.c
@@ -13,6 +13,19 @@ Obstacle createObstacle(Vector2 position, ObstacleType type) {
         case OBSTACLE_SHARK:
             obstacle.data.shark = createShark(position, (Vector2){0, 0});
             break;
+        case OBSTACLE_PIGEON:
+            obstacle.data.pigeon = createPigeon(position);
+            break;
+        case OBSTACLE_BUS:
+            obstacle.data.bus.position = position;
+            obstacle.data.bus.hitbox = (Rectangle){
+                position.x, 
+                position.y, 
+                150,
+                80
+            };
+            obstacle.data.bus.speed = 350.0f;
+            break;
         default:
             // Inicialize outros tipos de obstáculos conforme necessário
             break;
@@ -29,8 +42,23 @@ void updateObstacle(Obstacle* obstacle, Vector2 playerPosition, float deltaTime)
         case OBSTACLE_SHARK:
             updateShark(&obstacle->data.shark, playerPosition, deltaTime);
             break;
+        case OBSTACLE_BUS:
+            obstacle.data.bus.position = position;
+            obstacle.data.bus.hitbox = (Rectangle){
+                position.x, 
+                position.y, 
+                150,
+                80
+            };
+            obstacle.data.bus.speed = 350.0f;
+            break;
+        case OBSTACLE_PIGEON: {
+            Pigeon *pigeon = &obstacle->data.pigeon;
+            int shouldPoop = updatePigeon(pigeon, deltaTime);
+            (void)shouldPoop;
+            break;
+        }
         default:
-            // Atualize outros tipos de obstáculos conforme necessário
             break;
     }
 }
@@ -43,8 +71,23 @@ void drawObstacle(Obstacle obstacle, Texture2D crabTexture, Texture2D sharkTextu
         case OBSTACLE_SHARK:
             drawShark(obstacle.data.shark, sharkTexture);
             break;
+        case OBSTACLE_BUS: {
+            Bus bus = obstacle.data.bus;
+            DrawRectangleRec(bus.hitbox, YELLOW);
+            DrawRectangleLinesEx(bus.hitbox, 3, BLACK);
+            DrawRectangle(bus.hitbox.x + 20, bus.hitbox.y + 10, 25, 20, BLUE);
+            DrawRectangle(bus.hitbox.x + 60, bus.hitbox.y + 10, 25, 20, BLUE);
+            DrawRectangle(bus.hitbox.x + 100, bus.hitbox.y + 10, 25, 20, BLUE);
+            break;
+        }
+        case OBSTACLE_PIGEON:
+            drawPigeon(obstacle.data.pigeon);
+            break;
+        
+        case OBSTACLE_SHARK:
+            drawShark(obstacle.data.shark, sharkTexture);
+            break;
         default:
-            // Desenhe outros tipos de obstáculos conforme necessário
             break;
     }
 }
--- src/entities/obstacle.h
+++ src/entities/obstacle.h
@@ -4,17 +4,28 @@
 #include <raylib.h>
 #include "crab.h"
 #include "shark.h"
+#include "pigeon.h"
 
 typedef enum {
-    OBSTACLE_CRAB = 0,
-    OBSTACLE_SHARK = 1
+    OBSTACLE_CRAB = 0,         
+    OBSTACLE_POTHOLE = 1,       
+    OBSTACLE_PIGEON_POOP = 2,   
+    OBSTACLE_BUS = 3,           
+    OBSTACLE_PIGEON = 4,       
+    OBSTACLE_SHARK = 5         
 } ObstacleType;
 
 typedef struct {
     Vector2 position;
     ObstacleType type;
+    int active;
+    
     union {
         Crab crab;
+        Pothole pothole;
+        PigeonPoop pigeonPoop;
+        Bus bus;
+        Pigeon pigeon;        
         Shark shark;
     } data;
 } Obstacle;
